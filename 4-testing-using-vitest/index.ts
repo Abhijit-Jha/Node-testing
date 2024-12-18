55ef0fd15c417bfd7ee495811eb4639fabccffae
Abhijit-Jha <abhijeetjha913@gmail.com>
2024-12-18 17:40:14 +0530

    Basic test using vitest

---
+++ 4-testing-using-vitest/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+export const app = express();
+app.use(express.json());
+
+app.post("/sum", (req, res) => {
+    const body = req.body;
+    const answer = body.a + body.b;
+
+    res.json({
+        answer
+    }).status(200);
+});
